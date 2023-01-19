74f80e4f796beecbcbb614a51181f09f2d23b7e2
Balazs E. Pataki <pataki@gmail.com>
2023-01-19 12:52:26 +0100

    Add deleting DatasetFieldTypeOverride-s

--- src/main/java/edu/harvard/iq/dataverse/DatasetFieldTypeOverrideServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/DatasetFieldTypeOverrideServiceBean.java
@@ -11,6 +11,9 @@ import java.util.Optional;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
+/**
+ * Handles DatasetFieldTypeOverride records.
+ */
 @Stateless
 @Named
 public class DatasetFieldTypeOverrideServiceBean implements java.io.Serializable
@@ -27,47 +30,20 @@ public class DatasetFieldTypeOverrideServiceBean implements java.io.Serializable
         return query.getResultList();
     }
 
-    /**
-     * Sets @{@link DatasetFieldTypeOverride#generateOriginalField} value to @{code true } for each field in
-     * @{code overrides}, when the original field associated with the override cannot be found in any of @{code allMdbs}
-     * @param overrides dataset field overrides to process
-     * @param allMdbs all MDB-s to be presented to the user, including the MDB of the overrides
-     * @param dv the Dataverse the dataset with overriden fields belongs to. Used for calculating hidden fields.
-     * @return
-     */
-    public void calcGenerateOriginalFieldValues(
-            List<DatasetFieldTypeOverride> overrides,
-            List<MetadataBlock> allMdbs,
-            Dataverse dv
-    ) {
-        if (overrides.isEmpty()) {
-            return;
-        }
-        // Field that may be excluded from MDB-s
-        List<DataverseFieldTypeInputLevel> inputLevels = dv.getDataverseFieldTypeInputLevels();
-
-        MetadataBlock overrideMdb = overrides.get(0).getMetadataBlock();
-
-        // MDB-s other than that of overrides
-        List<MetadataBlock> otherMdbs = allMdbs.stream().filter(mdb -> mdb.getId() != overrideMdb.getId()).collect(Collectors.toList());
-
-        // Now set the generateOriginalField based on whether the original field is included in any of the otherMdbs
-        // and is not contained in the inputLevels with include==false value.
-        overrides.stream().forEach(ov -> {
-            boolean otherMdbContains = otherMdbs.contains(ov.getOriginal().getMetadataBlock());
-            // Even if other MDB contains, it may be hidden
-            if (otherMdbContains) {
-                // If the original field is listed in the Dataverse as not included then actually no other MDB will
-                // contain the field, ie. no input field will be generated for it
-                Optional<DataverseFieldTypeInputLevel> ilFound = inputLevels.stream().filter(il ->
-                        il.getDatasetFieldType().getId() == ov.getOriginal().getId()
-                ).findFirst();
-                if (ilFound.isPresent() && ilFound.get().isInclude() == false) {
-                    otherMdbContains = false;
-                }
-            }
-            // If no other mdb contains the original field, then we have to generate it when displaying overrideMdb
-            ov.setGenerateOriginalField(!otherMdbContains);
-        });
+    public DatasetFieldTypeOverride save(DatasetFieldTypeOverride override) {
+        return em.merge(override);
     }
+
+    public List<DatasetFieldTypeOverride> save(List<DatasetFieldTypeOverride> overrides) {
+        return overrides.stream().map(ov -> em.merge(ov)).collect(Collectors.toList());
+    }
+
+    public void delete(DatasetFieldTypeOverride override) {
+         em.remove(override);
+    }
+
+    public void delete(List<DatasetFieldTypeOverride> overrides) {
+        overrides.forEach(ov -> em.remove(ov));
+    }
+
 }
