c02095eed4d1c0c945aa69a6fb53f963be21e652
Balazs E. Pataki <pataki@gmail.com>
2023-11-24 21:44:48 +0100

    Add AromaServlet for serving webapp/aroma
    AromaServlet is needed to be able to server /aroma/index.html when requesting
    /aroma.

---
+++ src/main/java/edu/harvard/iq/dataverse/arp/AromaServlet.java
@@ -0,0 +1,42 @@
+package edu.harvard.iq.dataverse.arp;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.nio.file.Files;
+
+/**
+ * Servlet to server /aroma/index.html when /aroma is requested
+ */
+public class AromaServlet extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String basePath = getServletContext().getRealPath("/aroma");
+        String requestedResource = request.getPathInfo();
+
+        // Serve index.html for the base path of /aroma
+        if (requestedResource == null || requestedResource.equals("/")) {
+            requestedResource = "index.html";
+        }
+
+        File file = new File(basePath, requestedResource);
+
+        if (!file.exists()) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // Set content type and copy file contents to response
+        String contentType = getServletContext().getMimeType(file.getName());
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+        response.setContentType(contentType);
+        response.setContentLengthLong(file.length());
+        Files.copy(file.toPath(), response.getOutputStream());
+    }
+}
