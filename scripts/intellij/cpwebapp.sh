9b9b1a390010a828859f79695a460a0eab55d1a4
Balazs E. Pataki <pataki@gmail.com>
2024-01-19 12:28:41 +0100

    Update watcher config

--- scripts/intellij/cpwebapp.sh
+++ scripts/intellij/cpwebapp.sh
@@ -1,28 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # cpwebapp <project dir> <file in webapp>
 #
-# Use it either as an external tool and trigger via menu or shortcut, or add a File watcher by loading
-# watchers.xml into an IntelliJ IDE, and let it do the copying whenever you say a file under webapp.
+# Usage:
+#
+# Add a File watcher by importing watchers.xml into IntelliJ IDEA, and let it do the copying whenever you save a
+# file under webapp.
+#
+#     https://www.jetbrains.com/help/idea/settings-tools-file-watchers.html
+#
+# Alternatively, you can add an External tool and trigger via menu or shortcut to do the copying manually:
+#
+#     https://www.jetbrains.com/help/idea/configuring-third-party-tools.html
 #
 
 PROJECT_DIR=$1
 FILE_TO_COPY=$2
 RELATIVE_PATH="${FILE_TO_COPY#$PROJECT_DIR/}"
 
-#echo "project dir: |$$PROJECT_DIR|"
-#echo "File to copy: |$FILE_TO_COPY|"
-#echo "RELATIVE_PATH: |$RELATIVE_PATH|"
-
 # Check if RELATIVE_PATH starts with 'src/main/webapp', otherwise ignore
 if [[ $RELATIVE_PATH == src/main/webapp* ]]; then
-    # get current version
-    V=`perl -ne 'print $1 if /<revision>(.*?)<\/revision>/' ./modules/dataverse-parent/pom.xml`
-    #echo "Version: $V"
-
+    # Get current version. Any other way to do this? A simple VERSION file would help.
+    VERSION=`perl -ne 'print $1 if /<revision>(.*?)<\/revision>/' ./modules/dataverse-parent/pom.xml`
     RELATIVE_PATH_WITHOUT_WEBAPP="${RELATIVE_PATH#src/main/webapp/}"
-
-    TARGET_DIR=./docker-dev-volumes/glassfish/applications/dataverse-$V
+    TARGET_DIR=./docker-dev-volumes/glassfish/applications/dataverse-$VERSION
     TARGET_PATH="${TARGET_DIR}/${RELATIVE_PATH_WITHOUT_WEBAPP}"
 
     mkdir -p "$(dirname "$TARGET_PATH")"
