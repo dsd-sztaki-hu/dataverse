d4025779ca165130461e9c01fa33e50ddd10aeca
Balazs E. Pataki <pataki@gmail.com>
2024-02-13 14:57:41 +0100

    Initial dummy cookie consent support

---
+++ src/main/webapp/resources/arp/cookieconsent-config.js
@@ -0,0 +1,107 @@
+//created at: https://playground.cookieconsent.orestbida.com/
+import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@v3.0.0/dist/cookieconsent.umd.js';
+
+CookieConsent.run({
+    guiOptions: {
+        consentModal: {
+            layout: "box",
+            position: "middle center",
+            equalWeightButtons: false,
+            flipButtons: false
+        },
+        preferencesModal: {
+            layout: "box",
+            position: "right",
+            equalWeightButtons: false,
+            flipButtons: false
+        }
+    },
+    categories: {
+        necessary: {
+            readOnly: true
+        },
+        analytics: {}
+    },
+    language: {
+        default: localeCode, // comes from dataverse_template.xhtml
+        translations: {
+            hu: {
+                consentModal: {
+                    title: "Süti beállítások",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+                    acceptAllBtn: "Összes elfogadása",
+                    acceptNecessaryBtn: "Összes visszautasítása",
+                    showPreferencesBtn: "Testreszabás",
+                    footer: "<a href=\"#link\">Privacy Policy</a>\n<a href=\"#link\">Terms and conditions</a>"
+                },
+                preferencesModal: {
+                    title: "Beállítások testreszabása",
+                    acceptAllBtn: "Összes elfogadása",
+                    acceptNecessaryBtn: "Összes visszautasítása",
+                    savePreferencesBtn: "Beállítások mentése",
+                    closeIconLabel: "Bezárás",
+                    serviceCounterLabel: "Szolgáltatások",
+                    sections: [
+                        {
+                            title: "Sütik használata",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                        },
+                        {
+                            title: "Szolgáltatás használatához elengedhetetlen sütik <span class=\"pm__badge\">Mindig</span>",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            linkedCategory: "necessary"
+                        },
+                        {
+                            title: "Analitikai sütik",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            linkedCategory: "analytics"
+                        },
+                        {
+                            title: "További információk",
+                            description: "További információkért érdeklődjön <a class=\"cc__link\" href=\"#yourdomain.com\">itt</a>."
+                        }
+                    ]
+                }
+            },
+            en: {
+                consentModal: {
+                    title: "Cookie settings",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+                    acceptAllBtn: "Accept all",
+                    acceptNecessaryBtn: "Reject all",
+                    showPreferencesBtn: "Manage preferences",
+                    footer: "<a href=\"#link\">Privacy Policy</a>\n<a href=\"#link\">Terms and conditions</a>"
+                },
+                preferencesModal: {
+                    title: "Cookie Preferences Center",
+                    acceptAllBtn: "Accept all",
+                    acceptNecessaryBtn: "Reject all",
+                    savePreferencesBtn: "Save preferences",
+                    closeIconLabel: "Close modal",
+                    serviceCounterLabel: "Service|Services",
+                    sections: [
+                        {
+                            title: "Cookie Usage",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                        },
+                        {
+                            title: "Strictly Necessary Cookies <span class=\"pm__badge\">Always Enabled</span>",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            linkedCategory: "necessary"
+                        },
+                        {
+                            title: "Analytics Cookies",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            linkedCategory: "analytics"
+                        },
+                        {
+                            title: "More information",
+                            description: "For any query in relation to my policy on cookies and your choices, please <a class=\"cc__link\" href=\"#yourdomain.com\">contact me</a>."
+                        }
+                    ]
+                }
+            }
+        }
+    },
+    disablePageInteraction: true
+});
