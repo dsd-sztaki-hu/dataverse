e1cc68a1984d5a7acd73a4c78d6adba80e139eb7
fintanorbert <finta@sztaki.hu>
2025-09-16 14:28:46 +0200

    Fix file entity generation bug due to lib upgrade - refs #26122

--- src/main/java/edu/harvard/iq/dataverse/arp/rocrate/RoCrateExportManager.java
+++ src/main/java/edu/harvard/iq/dataverse/arp/rocrate/RoCrateExportManager.java
@@ -962,6 +962,9 @@ public class RoCrateExportManager {
         }
         var file = fileEntityBuilder.build();
         roCrate.addDataEntity(file);
+        if (!toHasPart) {
+            roCrate.getRootDataEntity().removeFromHasPart(file.getId());
+        }
 
         return fileId;
     }
